b6cd02a825533c8b712819f582e628d0396c51dc
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-03-25 16:40:39 -0300

    Router Cart

---
+++ routes/cart.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/cart", (req, res) => {
+  res.send({ message: "productos en el carrito" });
+});
+
+export default router;
